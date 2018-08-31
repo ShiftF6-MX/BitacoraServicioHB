30ca7fa0482e7073d14f8b88a43b06aaebae72ee
IsraelSalazar <israel.salazar@gruposhift-f6.com.mx>
2018-08-31 16:18:15 -0500

    Actualización de clase técnico

--- src/mx/shf6/SMB/model/Tecnico.java
+++ src/mx/shf6/SMB/model/Tecnico.java
@@ -1,5 +1,58 @@
 package mx.shf6.SMB.model;
 
-public class Tecnico {
+import javafx.beans.property.ObjectProperty;
+import javafx.beans.property.SimpleObjectProperty;
+import javafx.beans.property.SimpleStringProperty;
+import javafx.beans.property.StringProperty;
 
-}
+public class Tecnico {
+	//PROPIEDADES
+	private ObjectProperty<Integer> sysPk; 		//Sys_PK (INT) | PrimaryKey
+	private StringProperty nombre; 				//Nombre (VARCHAR)
+	
+	//CONSTRUCTOR SIN PARAMETROS
+	public Tecnico() {
+		this(0,"");
+	}//FIN METODO
+	
+	//CONSTRUCTOR CON PARAMETROS
+	public Tecnico(Integer sysPk, String nombre) {
+		this.sysPk = new SimpleObjectProperty<Integer>(sysPk);
+		this.nombre = new SimpleStringProperty(nombre);
+	}//FIN METODO
+	
+	//METODOS PARA ACCESO A "SYSPK"
+	public void setSysPk(Integer sysPK) {
+		this.sysPk.set(sysPK);
+	}//FIN METODO
+	
+	public Integer getSysPk() {
+		return this.sysPk.get();
+	}//FIN METODO
+	
+	public ObjectProperty<Integer> sysPkProperty() {
+		return this.sysPk;
+	}//FIN METODO
+	//FIN METODOS "SYSPK"
+	
+	//METODOS PARA ACCESO A "NOMBRE"
+	public void setNombre(String nombre) {
+		this.nombre.set(nombre);
+	}//FIN METODO
+	
+	public String getNombre() {
+		return this.nombre.get();
+	}//FIN METODO
+	
+	public StringProperty nombreroperty() {
+		return this.nombre;
+	}//FIN METODO
+	//FIN METODOS "REFERENCIA"
+	
+	public String showInformacion() {
+		String informacionVenta = "DATOS DEL CLIENTE:\n Sys_PK: " + this.getSysPk() + "\n"
+				+ "Nombre: " + this.getNombre();
+		return informacionVenta;
+	}//FIN METODO
+
+}//FIN CLASE
